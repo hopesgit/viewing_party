c9682e6d4630f9652fb24b28d43bc808a10f9887
Hope Gochnour <hope.gochnour@gmail.com>
2020-12-01 14:54:50 -0700

    Reformatted dashboard index spec
    Co-Authored-By: Taylor Phillips <taphill@users.noreply.github.com>
    Co-Authored-By: null <JBelta@users.noreply.github.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -13,9 +13,7 @@ describe "As a visitor" do
       expect(page).to have_content("Your favorite destination for watching movies with friends! Register or sign in to get started.")
     end
 
-    it "There's a form to log in and a link to register" do
-      expect(page).to have_link("New to Viewing Party? Register Here")
-
+    it "There's a form to log in" do
       fill_in("Email", with: "hope@example.com")
       fill_in("Password", with: "Password")
       expect(page).to have_field("Email", with: "hope@example.com" )
@@ -23,6 +21,13 @@ describe "As a visitor" do
       click_button "Sign In"
 
       expect(current_path).to eq("/dashboard")
+    end
+
+    it "There's a link to register" do
+      expect(page).to have_link("New to Viewing Party? Register Here")
+
+      click_link("New to Viewing Party? Register Here")
+      expect(current_path).to eq("/register")
     end
   end
 end
