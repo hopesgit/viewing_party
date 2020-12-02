537314dfe90bf81fc2dc879ed8c87fd102b68be0
Hope Gochnour <hope.gochnour@gmail.com>
2020-12-02 14:00:41 -0700

    Added login functionality and tested same
    Co-Authored-By: James Belta <JBelta@users.noreply.github.com>
    Co-Authored-By: Taylor Phillips <taylorscottphillips@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    redirect_to '/dashboard'
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in successfully."
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Email or Password doesn't match our records."
+      redirect_to root_path
+    end
   end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "As a visitor" do
   before :each do
-    visit "/"
+    visit root_path
   end
   describe "When I visit the root page" do
     it "I see the name of the site" do
@@ -13,6 +13,17 @@ describe "As a visitor" do
       expect(page).to have_content("Your favorite destination for watching movies with friends! Register or sign in to get started.")
     end
 
+    it "There's a link to register" do
+      expect(page).to have_link("New to Viewing Party? Register Here")
+
+      click_link("New to Viewing Party? Register Here")
+      expect(current_path).to eq("/register")
+    end
+
+    before :each do
+      @user1 = User.create!(username: "hopegochnour", email: "hope@example.com", password: "Password")
+    end
+
     it "There's a form to log in" do
       fill_in("Email", with: "hope@example.com")
       fill_in("Password", with: "Password")
@@ -21,13 +32,19 @@ describe "As a visitor" do
       click_button "Sign In"
 
       expect(current_path).to eq("/dashboard")
+      expect(page).to have_content("Logged in successfully.")
+      expect(page).to have_content("Welcome #{@user1.username}")
     end
 
-    it "There's a link to register" do
-      expect(page).to have_link("New to Viewing Party? Register Here")
+    it "Can't log in with bad credentials" do
+      fill_in("Email", with: "#{@user1.email}")
+      fill_in("Password", with: "0")
+      save_and_open_page
+      click_button "Sign In"
+      save_and_open_page
 
-      click_link("New to Viewing Party? Register Here")
-      expect(current_path).to eq("/register")
+      expect(current_path).to_not eq("/dashboard")
+      expect(page).to have_content("Email or Password doesn't match our records.")
     end
   end
 end
