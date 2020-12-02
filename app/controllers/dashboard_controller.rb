3859766e78c4dd796ae71e3bdead2986d45babc1
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-01 19:50:37 -0600

    Create current_user helper method
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,4 @@
 class DashboardController < ApplicationController
-  # before_action :require_user
-
   def show
-    @user = User.find(session[:user_id])
   end
-
-  # private
-  # def require_user
-  #
-  # end
 end
