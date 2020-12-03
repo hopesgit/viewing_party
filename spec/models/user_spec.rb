4fe3e56176adf0f05a5b6e8113b3e6e4f1c57469
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-03 14:38:02 -0600

    Add User model tests
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
   end
+
+  describe 'relationships' do
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
+    it { should have_many(:inverse_friendships) }
+    it { should have_many(:inverse_friends).through(:inverse_friendships) }
+  end
 end
