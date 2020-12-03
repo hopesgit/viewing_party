b0d4bf46cf39ac21181ed8f40f59f4ac7ff6fdfd
James Belta <james.belta@gmail.com>
2020-12-02 23:12:09 -0700

    Friend ship many-to-many self reference table
    Co-Authored-By: Taylor Phillips <taphill@users.noreply.github.com>
    Co-Authored-By: Hope Gochnour <hopesgit@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_233558) do
+ActiveRecord::Schema.define(version: 2020_12_02_230542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
