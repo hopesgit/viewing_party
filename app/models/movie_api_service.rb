52cb31234abb160773cc57f27254624e64d13b45
Hope Gochnour <hope.gochnour@gmail.com>
2020-12-05 12:01:13 -0700

    Add specs and methods for searching thdb api for movies by keywords

--- app/models/movie_api_service.rb
+++ app/models/movie_api_service.rb
@@ -1,29 +1,37 @@
 class MovieAPIService
   def conn
     Faraday.new(
-      url: "https://api.themoviedb.org/3/movie",
+      url: "https://api.themoviedb.org/3/",
       params: {api_key: ENV['tmdb_api_key']}
     )
   end
 
-  def get_top_rated_page_1
-    response1 = conn.get('top_rated') do |c|
-      c.params["page"] = 1
+  def get_top_rated_page(page_number)
+    response = conn.get('movie/top_rated') do |c|
+      c.params["page"] = page_number
     end
-    JSON.parse(response1.body, symbolize_names: true)[:results]
-  end
-
-  def get_top_rated_page_2
-    response2 = conn.get('top_rated') do |c|
-      c.params["page"] = 2
-    end
-    JSON.parse(response2.body, symbolize_names: true)[:results]
+    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
   def get_top_rated
     top_movies = []
-    top_movies << get_top_rated_page_1
-    top_movies << get_top_rated_page_2
+    top_movies << get_top_rated_page(1)
+    top_movies << get_top_rated_page(2)
     top_movies.flatten
+  end
+
+  def find_movies_by_title(search, page_number)
+    response = conn.get('search/movie') do |c|
+      c.params["page"] = page_number
+      c.params["query"] = search
+    end
+    JSON.parse(response.body, symbolize_names: true)[:results]
+  end
+
+  def search_movies_by_keyword(search)
+    movies_from_search = []
+    movies_from_search << find_movies_by_title(search, 1)
+    movies_from_search << find_movies_by_title(search, 2)
+    movies_from_search.flatten
   end
 end
--- spec/models/movie_api_service_spec.rb
+++ spec/models/movie_api_service_spec.rb
@@ -9,18 +9,22 @@ describe MovieAPIService, type: :class do
       expect(@api.conn.class).to eq(Faraday::Connection)
     end
 
-    it 'get_top_rated_page_1', :vcr do
-      expect(@api.get_top_rated_page_1.count).to eq(20)
-      expect(@api.get_top_rated_page_1[0][:original_title]).to eq("Gabriel's Inferno Part III")
-    end
-
-    it 'get_top_rated_page_2', :vcr do
-      expect(@api.get_top_rated_page_2.count).to eq(20)
-      expect(@api.get_top_rated_page_2[0][:original_title]).to eq("Forrest Gump")
+    it 'get_top_rated_page(page_number)', :vcr do
+      expect(@api.get_top_rated_page(1).count).to eq(20)
+      expect(@api.get_top_rated_page(1)[0][:original_title]).to eq("Gabriel's Inferno Part III")
     end
 
     it 'get_top_rated', :vcr do
       expect(@api.get_top_rated.count).to eq(40)
+    end
+
+    it 'find_movies_by_title(search, page_number)', :vcr do
+      expect(@api.find_movies_by_title("blue", 1).count).to eq(20)
+      expect(@api.find_movies_by_title("blue", 1)[0][:original_title]).to eq("Black and Blue")
+    end
+
+    it 'search_movies_by_keyword(search)', :vcr do
+      expect(@api.search_movies_by_keyword("blue").count).to eq(40)
     end
   end
 end
