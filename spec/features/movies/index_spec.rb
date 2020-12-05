119528cfd13252e6dedc79ac0fb676228f226b69
Hope Gochnour <hope.gochnour@gmail.com>
2020-12-05 12:49:47 -0700

    Add testing for the movies index/search page
    Co-Authored-By: Taylor Phillips <taylorscottphillips@gmail.com>
    Co-Authored-By: James Belta <JBelta@users.noreply.github.com>

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Movies/Index", type: :feature do
+describe "As a user" do
+  describe "when I visit /movies" do
+    before :each do
+      @user = User.create(username: 'John', email: 'Example@email.com', password: 'cool')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit "/movies"
+    end
+
+    it "displays a list of movies and their vote averages", :vcr do
+      within(first(".movie")) do
+        expect(page).to have_css(".title")
+        expect(page).to have_css(".rating")
+      end
+
+      expect(page).to have_css(".movie", count: 40)
+    end
+
+    it "has a field to type in a title and search by that", :vcr do
+      # within(first(".movie")) do
+      #   within(".title") do
+      #     expect(page).to have_content("Gabriel's Inferno Part III")
+      #   end
+      # end
+
+      fill_in(:movie_title, with: "down")
+      click_button("Find Movies")
+
+      within(first(".movie")) do
+        within(".title") do
+          expect(page).to_not have_content("Gabriel's Inferno Part III")
+        end
+      end
+    end
+  end
 end
