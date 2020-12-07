ac8ec23df3e1053e91a9307e1c9ffbf387a88949
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-07 17:02:03 -0600

    Add tests for movie/show page
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -11,7 +11,9 @@ describe "As a User" do
     it "Has a button to create a viewing party", :vcr do
       click_on 'The Godfather'
 
-      expect(page).to have_button("Create Viewing Party for Movie")
+      # expect(page).to have_button("Create Viewing Party for Movie")
+      click_on 'Create Viewing Party for Movie'
+      expect(page).to have_current_path(viewing_party_new_path)
     end
 
     it "Has movie details: Title, Vote ANG, Runtime, Genere's, Summary, first 10 cast members, Review count w author and info.", :vcr do
@@ -23,6 +25,19 @@ describe "As a User" do
         expect(page).to have_content("8.7")
         expect(page).to have_content("175")
         expect(page).to have_content("Drama Crime")
+      end
+
+      within('.summary') do
+        expect(page).to have_css('#movie-summary')
+      end
+
+      within('.cast') do
+        expect(page).to have_css('#cast-member', count: 10) 
+      end
+
+      within('.reviews') do
+        expect(page).to have_css('.review-author') 
+        expect(page).to have_css('.review-content') 
       end
     end
   end
