74b53666f66c360bec5727abe38643fba7c5a3fc
James Belta <james.belta@gmail.com>
2020-12-07 14:28:21 -0700

    removed pry's
    Co-Authored-By: Taylor Phillips <taphill@users.noreply.github.com>
    Co-Authored-By: Hope Gochnour <hopesgit@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,6 +7,7 @@ describe "As a User" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
       visit movies_path
     end
+
     it "Has a button to create a viewing party", :vcr do
       within(first(".movie")) do
         click_link "Gabriel's Inferno Part III"
@@ -16,14 +17,12 @@ describe "As a User" do
     end
 
     it "Has movie details: Title, Vote ANG, Runtime, Genere's, Summary, first 10 cast members, Review count w author and info.", :vcr do
-      binding.pry
       within(first(".movie")) do
         click_link "Gabriel's Inferno Part III"
       end
       expect(page).to have_content("Gabriel's Inferno Part III")
 
       within('.general-information') do
-        binding.pry
         expect(page).to have_content("")
         expect(page).to have_content("")
         expect(page).to have_content("")
