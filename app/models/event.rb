5ad0641383679225e888fb47bb5a3b1a7ef9888f
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-08 11:41:12 -0600

    Create Event model
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
-class ViewingParty < ApplicationRecord
-  belongs_to :movie, :user
+class Event < ApplicationRecord
+  belongs_to :user
+  belongs_to :movie
   validates_presence_of :user_id
   validates_presence_of :movie_id
   validates_presence_of :start_time
