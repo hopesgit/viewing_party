f1645601a3cc11af17cf335032f937119d0646f0
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-09 17:53:17 -0600

    Create Participant model
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+  has_many :participants
   validates :user_id, presence: true
   validates :movie_id, presence: true
   validates :start_time, presence: true
