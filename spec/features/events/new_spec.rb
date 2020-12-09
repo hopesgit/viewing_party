6df36c13d25171c895a7f5d605dbd51c791a4e92
James Belta <james.belta@gmail.com>
2020-12-08 20:20:53 -0700

    :each included

--- spec/features/events/new_spec.rb
+++ spec/features/events/new_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Events/New", type: :feature do
   describe 'As an authenticated user' do
     context 'when I visit events/new' do
-      before do
+      before :each do
         @user1 = User.create(username: 'John', email: 'Example@email.com', password: 'cool')
         @user2 = User.create!(username: 'jsmith', email: 'jsmith@example.com', password: 'password')
         @user3 = User.create!(username: 'tphill', email: 'tphill@example.com', password: 'password')
@@ -21,9 +21,26 @@ RSpec.describe "Events/New", type: :feature do
       end
 
       it 'can create a new event', :vcr do
-        fill_in :duration, with: 190
-        # select_datetime("2020, December, 20, 05 PM, 00", from: :start_time)
+        fill_in "event[duration]", with: 190
+        select("2022", from: "event[start_time(1i)]")
+        select("May", from: "event[start_time(2i)]")
+        select("3", from: "event[start_time(3i)]")
+        select("05 PM", from: "event[start_time(4i)]")
+        select("30", from: "event[start_time(5i)]")
         # find(:css, "friend-#{@user2.id}").set(true)
+
+        click_on "Create Event"
+        expect(current_path).to eq(dashboard_path)
+
+        #event = Event.last
+
+        within("#viewing-parties") do
+          within("#host") do
+            expect(page).to have_content("The Godfather")
+            expect(page).to have_content("Date")
+            expect(page).to have_content("Host Status")
+          end
+        end
       end
     end
   end
