a13bc20ed8e3f7d1d0ac75c8d05ca519f21a56f7
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-09 17:59:35 -0600

    Create events_invited_to metehod in User model
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :inverse_friends, through: :inverse_friendships, source: :user
   has_many :events
+  has_many :participants 
 
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
 
   has_secure_password
+
+  def events_invited_to
+    participants.map do |participant|
+      Event.find(participant.event_id) 
+    end
+  end
 end
