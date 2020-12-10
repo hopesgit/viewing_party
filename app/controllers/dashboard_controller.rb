98c251e24ee94b8d2c29c977e478c7c52528cba8
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-09 20:52:42 -0600

    Add upcoming movies to user dashboard
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: James Belta <41242546+jbelta@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def show
+    api = MovieAPIService.new
     @friends = current_user.friends
+    @upcoming_movies = api.upcoming_movies
   end
 end
